c1edcaeaafb5ba5fab6cf86ff26cabae07da2496
sebastianromane <sebastianromane@gmail.com>
2022-08-06 23:58:14 -0400

    declaration of variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//declaration of variables.
+let inputAmount= document.querySelector("#inputAmount");
+let selectTypeMoney= document.querySelector ("#selectTypeMoney");
+let btn= document.querySelector ("#btn");
+let dataArray = [];
+let myChart = null;
+let lastchange = 0;
